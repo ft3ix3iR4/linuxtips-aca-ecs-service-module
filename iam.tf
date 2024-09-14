d5681871bc78a01b6580333f32998ec6f6476fd7
Fellipe De Oliveira Menezes Teixeira <fellipe.teixeira@external.engie.com>
2024-09-13 21:47:17 -0300

    setup IAM role

--- iam.tf
+++ iam.tf
@@ -1,22 +1,26 @@
 resource "aws_iam_role" "service_execution_role" {
   name = format("%s-%s-service-role", var.cluster_name, var.service_name)
 
-  assume_role_policy = jsondecode({
-    Version - "2022-10-17"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [
+      {
         Action = "sts:AssumeRole"
         Principal = {
-            Service = "ecs.amazonaws.com"
+          Service = "ecs-tasks.amazonaws.com"
         }
         Effect = "Allow"
         Sid    = ""
+      },
     ]
   })
+
 }
 
-resource "aws_iam_role_policy" "name" {
+resource "aws_iam_role_policy" "service_execution_role" {
   name = format("%s-%s-service-policy", var.cluster_name, var.service_name)
   role = aws_iam_role.service_execution_role.id
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
