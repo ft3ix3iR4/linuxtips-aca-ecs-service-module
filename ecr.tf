2f25534fabbeb5a6cd3c6d171e528a4b536db178
Fellipe De Oliveira Menezes Teixeira <fellipe.teixeira@external.engie.com>
2024-09-13 22:15:53 -0300

    Setup ECR and CloudWatch

--- ecr.tf
+++ ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "main" {
-  name         = format("%ss/%s", var.cluster_name, var.service_name)
+  name         = format("%s/%s", var.cluster_name, var.service_name)
   force_delete = true
   image_scanning_configuration {
     scan_on_push = true
