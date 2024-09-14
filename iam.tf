bdedfdc3775b3854b2c30d1b216f781760e934fe
ft3ix3iR4 <fellipewolly2@gmail.com>
2024-09-14 18:53:23 -0300

    fix roles

--- iam.tf
+++ iam.tf
@@ -32,7 +32,7 @@ resource "aws_iam_role_policy" "service_execution_role" {
           "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
           "elasticloadbalancing:RegisterTargets",
           "ec2:Describe*",
-          "ec2:AuthorizeSecurityGroupIngress"
+          "ec2:AuthorizeSecurityGroupIngress",
           "ecr:GetAuthorizationToken",
           "ecr:BatchCheckLayerAvailability",
           "ecr:GetDownloadUrlForLayer",
