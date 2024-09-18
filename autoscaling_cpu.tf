bf232c06891d19fe7fbc06a7acf3ea5f4ee22a9f
ft3ix3iR4 <fellipewolly2@gmail.com>
2024-09-17 21:12:11 -0300

    setup autoscaling_cpu

--- autoscaling_cpu.tf
+++ autoscaling_cpu.tf
@@ -1,8 +1,8 @@
 resource "aws_appautoscaling_policy" "cpu_high" {
   count = var.scale_type == "cpu" ? 1 : 0
 
-  resource_id = aws_appautoscaling_target.main.resource_id
-  service_namespace = aws_appautoscaling_target.main.service_namespace
+  resource_id        = aws_appautoscaling_target.main.resource_id
+  service_namespace  = aws_appautoscaling_target.main.service_namespace
   scalable_dimension = aws_appautoscaling_target.main.scalable_dimension
 
   name = format("%s-%s-cpu-scale-out", var.cluster_name, var.service_name)
@@ -10,13 +10,13 @@ resource "aws_appautoscaling_policy" "cpu_high" {
   policy_type = "StepScaling"
 
   step_scaling_policy_configuration {
-    adjustment_type = "ChangeInCapacity"
-    cooldown = var.scale_out_cooldown
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = var.scale_out_cooldown
     metric_aggregation_type = var.scale_out_statistic
 
     step_adjustment {
       metric_interval_lower_bound = 0
-      scaling_adjustment = var.scale_out_adjustment
+      scaling_adjustment          = var.scale_out_adjustment
     }
   }
 }
@@ -29,12 +29,12 @@ resource "aws_cloudwatch_metric_alarm" "cpu_high" {
   comparison_operator = var.scale_out_comparison_operator
 
   metric_name = "CPUUtilization"
-  namespace = "AWS/ECS"
-  statistic = var.scale_out_statistic
+  namespace   = "AWS/ECS"
+  statistic   = var.scale_out_statistic
 
-  period = var.scale_out_period
+  period             = var.scale_out_period
   evaluation_periods = var.scale_out_evaluation_periods
-  threshold = var.scale_out_cpu_threshold
+  threshold          = var.scale_out_cpu_threshold
 
   dimensions = {
     ClusterName = var.cluster_name
